61dc05534ac2acc1ccdb254612fee6dc8dfeb382
Repkit <ionut.baches@yahoo.com>
2016-12-07 11:44:43 +0200

    Create is-not-a-function.js
    Uncaught TypeError: timestamp is not a function(…)

---
+++ js/is-not-a-function.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function timestamp() {
+
+	return Math.floor(Date.now() / 1000);
+
+}
+
+function doSomethingSmart() {
+
+  var ts = timestamp();
+  var timestamp = "something";
+  
+  return true;
+  
+}
